0c32c914f46c6f4f927b4215ae400cb38eebfcb5
Miguel <whytemig@gmail.com>
2023-07-30 00:31:36 -0400

    notes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import { useState } from "react";
 import Header from "./components/Header";
-import Task from "./components/Task";
+import Task from "./components/Task"
+import Add from "./components/Add";
 
 
 function App() {
   const [tasks, setTasks ] = useState([
     {
       id: 1,
-      test: 'whatever',
+      text: 'whatever',
       day: 'Feb 5th at 2:30pm',
       reminder: true
     },
     {
       id: 2,
-      test: 'whatever2',
+      text: 'whatever2',
       day: 'Aug 5th at 8:45pm',
       reminder: true
     }
   ])
+
+  // Add Function 
+
+  const addTask = (task) =>{
+    const id = Math.floor(Math.random() * 1000) + 1
+
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
 
 
   // Delete Task
@@ -34,6 +44,7 @@ const toggleReminder = (id)=>{
   return (
     <div className="container">
      <Header />
+     <Add onAdd={addTask}/>
      {tasks.length > 0 ? <Task tasks={tasks}  onDelete={deleteTask} onToggle={toggleReminder}/> : 'No more Taske'}
     </div>
   )
--- src/components/Task2.js
+++ src/components/Task2.js
@@ -4,7 +4,7 @@ import {FaTimes} from 'react-icons/fa'
 const Task2 = ({task, onDelete, onToggle}) => {
   return (
     <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={()=>onToggle(task.id)}>
-      <h3>{task.test} <FaTimes style={{color:'red', cursor: 'pointer'}} onClick={()=> onDelete(task.id)}/></h3>  
+      <h3>{task.text} <FaTimes style={{color:'red', cursor: 'pointer'}} onClick={()=> onDelete(task.id)}/></h3>  
       <p>{task.day}</p>  
     </div>
   )
